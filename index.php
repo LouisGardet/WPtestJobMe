557c3be8e05e38529731954209bbda5c2f62d3b4
Louis Gardet <louisegardet@gmail.com>
2023-05-10 18:19:38 +0200

    resolved new conflicts index

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
-<<<<<<< HEAD
-  if (!empty($_GET['q'])) {
-    switch ($_GET['q']) {
-      case 'info':
-        phpinfo(); 
-        exit;
-      break;
-      default:
-    }
+if (!empty($_GET['q'])) {
+  switch ($_GET['q']) {
+    case 'info':
+      phpinfo(); 
+      exit;
+    break;
+    default:
   }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -79,21 +78,3 @@
         </div>
     </body>
 </html>
-=======
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
->>>>>>> origin/master
